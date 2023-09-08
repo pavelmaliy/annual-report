a22b603dbacf06f74164b025519164e0a7bc9dd8
pavelmaliy <pavel.maliy@sap.com>
2023-09-08 14:10:56 +0300

    add tabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import Checkout from "./components/Checkout";
-import { useState } from 'react';
 
+export default function LabTabs() {
+    const [value, setValue] = React.useState('1');
+    const [model, setModel] = React.useState({});
 
-const App = () => {
-    const [model, setModel] = useState({});
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
-        <div>
-            <Checkout model={model} setModel={setModel}/>
-        </div>
+        <Box sx={{ width: '100%', typography: 'body1' }}>
+            <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
+                        <Tab label="Home" value="1" />
+                        <Tab label="Transactions" value="2" />
+                        <Tab label="Report" value="3" />
+                    </TabList>
+                </Box>
+                <TabPanel value="1">Home</TabPanel>
+                <TabPanel value="2">
+                    <Checkout model={model} setModel={setModel}/>
+                </TabPanel>
+                <TabPanel value="3">Report</TabPanel>
+            </TabContext>
+        </Box>
     );
-};
-
-export default App;
+}
