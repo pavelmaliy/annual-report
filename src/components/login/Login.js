fd26542076aa909fddb5a48b820a2bef8dc34f22
pavelmaliy <pavel.maliy@sap.com>
2023-09-13 09:30:52 +0300

    beautiful login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,18 +1,36 @@
-import React, {useEffect, useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {Google, Visibility, VisibilityOff} from "@mui/icons-material";
+import {InputAdornment} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import {useState, useEffect} from "react";
+import {useAuthState} from "react-firebase-hooks/auth";
 import { auth, logInWithEmailAndPassword, logInWithGoogle } from "../../storage/firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import "./Login.css";
-import GoogleButton from "react-google-button";
-function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+import {Link, useNavigate} from "react-router-dom";
+
+const defaultTheme = createTheme();
+
+export default function SignIn() {
     const [user, loading, error] = useAuthState(auth);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [rememberMe, setRememberMe] = useState(false);
+
     const navigate = useNavigate();
 
-
     useEffect(() => {
-        console.log("in login user:" + JSON.stringify(user) + "loading is " + loading)
         if (loading) {
             // maybe trigger a loading screen
             return;
@@ -21,43 +39,112 @@ function Login() {
             navigate("/dashboard");
         }
     }, [user, loading]);
+
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        logInWithEmailAndPassword(email, password)
+    };
+
     return (
-        <div className="login">
-            <div className="login__container">
-                <input
-                    type="text"
-                    className="login__textBox"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="E-mail Address"
-                />
-                <input
-                    type="password"
-                    className="login__textBox"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
-                />
-                <button
-                    className="login__btn"
-                    onClick={() => logInWithEmailAndPassword(email, password)}
-                >
-                    Login
-                </button>
-                <GoogleButton
-                    type="dark"
-                    onClick={async () => {
-                        await logInWithGoogle()
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
                     }}
-                />
-               <div>
-                    <Link to="/reset">Forgot Password</Link>
-                </div>
-                <div>
-                    Don't have an account? <Link to="/register">Register</Link> now.
-                </div>
-            </div>
-        </div>
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            autoFocus
+                        />
+                        <TextField
+                            id="password"
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Password"
+                            type={showPassword ? 'text' : 'password'}
+                            value={password}
+                            autoComplete="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                        >
+                                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <FormControlLabel name='remember'
+                            control={<Checkbox value={rememberMe} color="primary" onChange={(e) => {setRememberMe(e.target.checked)}} />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={() => logInWithEmailAndPassword(email, password)}
+                        >
+                            Sign In
+                        </Button>
+                        <Button
+                            startIcon={<Google/>}
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={async () => {
+                                await logInWithGoogle()
+                            }}
+                        >
+                            Login with Google
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link to="/reset">Forgot Password</Link>
+                            </Grid>
+                            <Grid item>
+                                <Link to="/register">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 }
-export default Login;
