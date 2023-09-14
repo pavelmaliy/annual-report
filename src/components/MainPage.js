b315ecede27b3490f0298ba9d0dba7182d1609af
pavelmaliy <pavel.maliy@sap.com>
2023-09-14 19:03:27 +0300

    menu

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,45 +4,69 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabPanel from '@mui/lab/TabPanel';
 import TransactionStepper from "./transactions/TransactionStepper";
-import {Tabs} from "@mui/material";
+import {Menu, MenuItem, Tabs} from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Dashboard from "./overview/Dashboard";
 import Typography from '@mui/material/Typography';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import IconButton from '@mui/material/IconButton';
-import {Stack} from "@mui/system";
+import MenuIcon from '@mui/icons-material/Menu';
 import {logout} from "../storage/firebase";
 import {useNavigate} from "react-router-dom";
+import Toolbar from "@mui/material/Toolbar";
 
 export default function MainPage({user}) {
     const [tab, setTab] = React.useState('1');
     const navigate = useNavigate();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = async (option) => {
+        setAnchorEl(null);
+        if (option === 'logout') {
+            try {
+                await logout()
+            } catch (e) {
+                throw e
+            }
+            navigate("/login");
+        }
+    };
+
     const handleTabChange = (event, newValue) => {
         setTab(newValue);
     };
-
-    const handleLogout = async () => {
-        try {
-            await logout()
-        } catch (e) {
-            throw e
-        }
-        navigate("/login");
-    }
 
     return (
         <Box sx={{width: '100%', typography: 'body1'}}>
             <TabContext value={tab}>
                 <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
                     <AppBar position="static">
-                        <Box pt={1} pr={2}>
-                            <Stack direction="column" alignItems="flex-end">
-                                <Typography sx={{ mb: -1, cursor: 'default'  }}>{user}</Typography>
-                                <IconButton color="inherit" onClick={handleLogout}>
-                                    <ExitToAppIcon />
-                                </IconButton>
-                            </Stack>
-                        </Box>
+                        <Toolbar>
+                            <IconButton
+                                edge="start"
+                                color="inherit"
+                                aria-label="menu"
+                                onClick={handleMenuOpen}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography variant="h6" style={{ flexGrow: 1 }}>
+                                {user}
+                            </Typography>
+                        </Toolbar>
+                        <Menu
+                            anchorEl={anchorEl}
+                            open={Boolean(anchorEl)}
+                            onClose={handleMenuClose}
+                        >
+                            <MenuItem onClick={()=> {handleMenuClose("logout")}}
+                            >
+                                Logout
+                            </MenuItem>
+                        </Menu>
                         <Tabs
                             value={tab}
                             onChange={handleTabChange}
