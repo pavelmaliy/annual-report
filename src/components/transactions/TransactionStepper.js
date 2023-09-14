4bb799115c3b5f9a6fbe2a54ee1430204ac57330
pavelmaliy <pavel.maliy@sap.com>
2023-09-14 20:19:03 +0300

    menu

--- src/components/transactions/TransactionStepper.js
+++ src/components/transactions/TransactionStepper.js
@@ -4,30 +4,27 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import TransactionForm from './TransactionForm';
 import GeneralInfo from "./GeneralInfo";
-import {Step, StepLabel, Stepper} from "@mui/material";
+import {
+    Step,
+    StepLabel,
+    Stepper, TableCell, TableHead, TableRow, TableContainer, Table, TableBody
+} from "@mui/material";
 import {persistTransactions} from "../../storage/store"
 import {AppContext} from "../../context/AppContext";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth} from "../../storage/firebase";
-
-function Copyright() {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
+import StarIcon from "@mui/icons-material/Star";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 const steps = ['General Info', 'Stock Transactions'];
+const tableContainerStyle = {
+    maxHeight: '300px', // Set the maximum height for vertical scrolling
+    overflowY: 'auto',   // Enable vertical scroll
+};
 
 export default function TransactionStepper() {
     const {model, setModel} = useContext(AppContext);
@@ -71,6 +68,9 @@ export default function TransactionStepper() {
             <CssBaseline/>
             <Container component="main" maxWidth="lg" sx={{mb: 4}}>
                 <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
+                    <Typography component="h1" variant="h4" align="center">
+                        New Transaction
+                    </Typography>
                     <Stepper activeStep={activeStep} sx={{pt: 3, pb: 5}}>
                         {steps.map((label) => (
                             <Step key={label}>
@@ -94,7 +94,7 @@ export default function TransactionStepper() {
                                 }}
                                 sx={{mt: 3, ml: 1}}
                             >
-                                New Transaction
+                                Finish
                             </Button>
                         </React.Fragment>
                     ) : (
@@ -103,7 +103,141 @@ export default function TransactionStepper() {
                         </React.Fragment>
                     )}
                 </Paper>
-                <Copyright/>
+            </Container>
+            <Container component="main" maxWidth="lg" sx={{mb: 4}}>
+                <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
+                    <Typography component="h1" variant="h4" align="center">
+                        Transaction History
+                    </Typography>
+                    <React.Fragment>
+                        <div style={tableContainerStyle}>
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell style={{ fontWeight: 'bold' }}>Date</TableCell>
+                                        <TableCell style={{ fontWeight: 'bold' }}>Stock</TableCell>
+                                        <TableCell style={{ fontWeight: 'bold' }}>Action</TableCell>
+                                        <TableCell style={{ fontWeight: 'bold' }}>Quantity</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>
+                                            <Typography>
+                                                15/10/2022
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                SAP
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                Sell
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                22
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>
+                                            <Typography>
+                                                15/10/2022
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                SAP
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                Sell
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                22
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>
+                                            <Typography>
+                                                15/10/2022
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                SAP
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                Sell
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                22
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>
+                                            <Typography>
+                                                15/10/2022
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                SAP
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                Sell
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                22
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>
+                                            <Typography>
+                                                15/10/2022
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                SAP
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                Sell
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography>
+                                                22
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        </div>
+                    </React.Fragment>
+                </Paper>
             </Container>
         </React.Fragment>
     );
