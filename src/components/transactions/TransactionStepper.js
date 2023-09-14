c502293de29823741421d536ee9fa4947b79e32c
pavelmaliy <pavel.maliy@sap.com>
2023-09-14 21:51:52 +0300

    menu

--- src/components/transactions/TransactionStepper.js
+++ src/components/transactions/TransactionStepper.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useContext} from 'react';
+import {useContext, useEffect} from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
@@ -15,10 +15,12 @@ import {
 import {persistTransactions} from "../../storage/store"
 import {AppContext} from "../../context/AppContext";
 import {useAuthState} from "react-firebase-hooks/auth";
-import {auth} from "../../storage/firebase";
+import {auth, db} from "../../storage/firebase";
 import StarIcon from "@mui/icons-material/Star";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import {collection, getDocs, query, where} from "firebase/firestore";
+import ReactLoading from "react-loading";
 
 const steps = ['General Info', 'Stock Transactions'];
 const tableContainerStyle = {
@@ -30,6 +32,24 @@ export default function TransactionStepper() {
     const {model, setModel} = useContext(AppContext);
     const [activeStep, setActiveStep] = React.useState(0);
     const [user] = useAuthState(auth)
+    const [history, setHistory] = React.useState([])
+    const [loading, setLoading] = React.useState(true);
+
+    useEffect(() => {
+        (async () => {
+            const q = query(collection(db, "transactions"), where("user_id", "==", user.uid));
+            try {
+                const docs = await getDocs(q);
+                docs.docs.map((item) => {
+                    history.push(item.data())
+                })
+            } catch (e) {
+                console.error(e)
+            }
+            setHistory(history)
+            setLoading(false);
+        })()
+    }, []);
 
     function getStepContent(step) {
         switch (step) {
@@ -64,181 +84,104 @@ export default function TransactionStepper() {
     }
 
     return (
-        <React.Fragment>
-            <CssBaseline/>
-            <Container component="main" maxWidth="lg" sx={{mb: 4}}>
-                <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
-                    <Typography component="h1" variant="h4" align="center">
-                        New Transaction
-                    </Typography>
-                    <Stepper activeStep={activeStep} sx={{pt: 3, pb: 5}}>
-                        {steps.map((label) => (
-                            <Step key={label}>
-                                <StepLabel>{label}</StepLabel>
-                            </Step>
-                        ))}
-                    </Stepper>
-                    {activeStep === steps.length ? (
-                        <React.Fragment>
-                            <Typography variant="h5" gutterBottom>
-                                Transactions successfully saved.
+        <div>
+            {(loading) ? (
+                <div className="loading-container">
+                    <ReactLoading type="spin" color="#0000FF"/>
+                </div>
+            ) : (
+                <React.Fragment>
+                    <CssBaseline/>
+                    <Container component="main" maxWidth="lg" sx={{mb: 4}}>
+                        <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
+                            <Typography component="h1" variant="h4" align="center">
+                                New Transaction
                             </Typography>
-                            <Button
-                                variant="contained"
-                                onClick={() => {
-                                    model.transactions = []
-                                    model.currency = ''
-                                    model.exchange = ''
-                                    setModel(model)
-                                    setActiveStep(0);
-                                }}
-                                sx={{mt: 3, ml: 1}}
-                            >
-                                Finish
-                            </Button>
-                        </React.Fragment>
-                    ) : (
-                        <React.Fragment>
-                            {getStepContent(activeStep, model, setModel, setActiveStep)}
-                        </React.Fragment>
-                    )}
-                </Paper>
-            </Container>
-            <Container component="main" maxWidth="lg" sx={{mb: 4}}>
-                <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
-                    <Typography component="h1" variant="h4" align="center">
-                        Transaction History
-                    </Typography>
-                    <React.Fragment>
-                        <div style={tableContainerStyle}>
-                        <TableContainer component={Paper}>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell style={{ fontWeight: 'bold' }}>Date</TableCell>
-                                        <TableCell style={{ fontWeight: 'bold' }}>Stock</TableCell>
-                                        <TableCell style={{ fontWeight: 'bold' }}>Action</TableCell>
-                                        <TableCell style={{ fontWeight: 'bold' }}>Quantity</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow>
-                                        <TableCell>
-                                            <Typography>
-                                                15/10/2022
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                SAP
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                Sell
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                22
-                                            </Typography>
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>
-                                            <Typography>
-                                                15/10/2022
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                SAP
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                Sell
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                22
-                                            </Typography>
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>
-                                            <Typography>
-                                                15/10/2022
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                SAP
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                Sell
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                22
-                                            </Typography>
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>
-                                            <Typography>
-                                                15/10/2022
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                SAP
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                Sell
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                22
-                                            </Typography>
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>
-                                            <Typography>
-                                                15/10/2022
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                SAP
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                Sell
-                                            </Typography>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Typography>
-                                                22
-                                            </Typography>
-                                        </TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                        </div>
-                    </React.Fragment>
-                </Paper>
-            </Container>
-        </React.Fragment>
+                            <Stepper activeStep={activeStep} sx={{pt: 3, pb: 5}}>
+                                {steps.map((label) => (
+                                    <Step key={label}>
+                                        <StepLabel>{label}</StepLabel>
+                                    </Step>
+                                ))}
+                            </Stepper>
+                            {activeStep === steps.length ? (
+                                <React.Fragment>
+                                    <Typography variant="h5" gutterBottom>
+                                        Transactions successfully saved.
+                                    </Typography>
+                                    <Button
+                                        variant="contained"
+                                        onClick={() => {
+                                            model.transactions = []
+                                            model.currency = ''
+                                            model.exchange = ''
+                                            setModel(model)
+                                            setActiveStep(0);
+                                        }}
+                                        sx={{mt: 3, ml: 1}}
+                                    >
+                                        Finish
+                                    </Button>
+                                </React.Fragment>
+                            ) : (
+                                <React.Fragment>
+                                    {getStepContent(activeStep, model, setModel, setActiveStep)}
+                                </React.Fragment>
+                            )}
+                        </Paper>
+                    </Container>
+                    <Container component="main" maxWidth="lg" sx={{mb: 4}}>
+                        <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
+                            <Typography component="h1" variant="h4" align="center">
+                                Transaction History
+                            </Typography>
+                            <React.Fragment>
+                                <div style={tableContainerStyle}>
+                                    <TableContainer component={Paper}>
+                                        <Table>
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell style={{fontWeight: 'bold'}}>Date</TableCell>
+                                                    <TableCell style={{fontWeight: 'bold'}}>Stock</TableCell>
+                                                    <TableCell style={{fontWeight: 'bold'}}>Action</TableCell>
+                                                    <TableCell style={{fontWeight: 'bold'}}>Quantity</TableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                                {history.map((item, index) => (
+                                                    <TableRow>
+                                                        <TableCell>
+                                                            <Typography>
+                                                                {item.transactionDate}
+                                                            </Typography>
+                                                        </TableCell>
+                                                        <TableCell>
+                                                            <Typography>
+                                                                {item.stockName}
+                                                            </Typography>
+                                                        </TableCell>
+                                                        <TableCell>
+                                                            <Typography>
+                                                                {item.transactionType}
+                                                            </Typography>
+                                                        </TableCell>
+                                                        <TableCell>
+                                                            <Typography>
+                                                                {item.quantity}
+                                                            </Typography>
+                                                        </TableCell>
+                                                    </TableRow>
+                                                ))}
+
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                </div>
+                            </React.Fragment>
+                        </Paper>
+                    </Container>
+                </React.Fragment>
+            )}
+        </div>
     );
 }
