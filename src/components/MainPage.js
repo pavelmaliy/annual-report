9c3ded8bd6f2d44d1fe30b69e5f11a4ff1348cf2
pavelmaliy <pavel.maliy@sap.com>
2023-09-15 16:32:31 +0300

    side bar

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,108 +1,240 @@
 import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabPanel from '@mui/lab/TabPanel';
-import TransactionStepper from "./transactions/TransactionStepper";
-import {Menu, MenuItem, Tabs} from "@mui/material";
-import AppBar from "@mui/material/AppBar";
-import Dashboard from "./overview/Dashboard";
-import Typography from '@mui/material/Typography';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import TransactionStepper from './transactions/TransactionStepper'
+import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import Dashboard from "./overview/Dashboard";
+import LogoutIcon from '@mui/icons-material/Logout';
 import {logout} from "../storage/firebase";
 import {useNavigate} from "react-router-dom";
-import Toolbar from "@mui/material/Toolbar";
-import {useEffect} from "react";
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import ArticleIcon from '@mui/icons-material/Article';
+import Typography from "@mui/material/Typography";
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
 
-export default function MainPage({user}) {
-    const [tab, setTab] = React.useState(localStorage.getItem('lastActiveTab') || '1');
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+);
+
+export default function MainPage() {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+    const [mainContent, setMainContent] = React.useState(<Dashboard/>)
     const navigate = useNavigate();
-    const [anchorEl, setAnchorEl] = React.useState(null);
 
-    useEffect(() => {
-        const storedValue = localStorage.getItem('lastActiveTab');
-        if (storedValue) {
-            setTab(storedValue)
+    const handleLogout = async () => {
+        try {
+            await logout()
+        } catch (e) {
+            throw e
         }
-    }, []);
+        navigate("/login");
+    }
 
-    useEffect(() => {
-        localStorage.setItem('lastActiveTab', tab);
-    }, [tab]);
-
-    const handleMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget);
+    const handleDrawerOpen = () => {
+        setOpen(true);
     };
 
-    const handleMenuClose = async (option) => {
-        setAnchorEl(null);
-        if (option === 'logout') {
-            try {
-                await logout()
-            } catch (e) {
-                throw e
-            }
-            navigate("/login");
-        }
-    };
-
-    const handleTabChange = (event, newValue) => {
-        setTab(newValue);
+    const handleDrawerClose = () => {
+        setOpen(false);
     };
 
     return (
-        <Box sx={{width: '100%', typography: 'body1'}}>
-            <TabContext value={tab}>
-                <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-                    <AppBar position="static">
-                        <Toolbar style={{ marginLeft: 'auto' }}>
-                            <Typography style={{ flexGrow: 1, cursor: 'default' }}>
-                                {user}
-                            </Typography>
-                            <IconButton
-                                style={{ paddingLeft: '16px' }}
-                                edge="start"
-                                color="inherit"
-                                aria-label="menu"
-                                onClick={handleMenuOpen}
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{
+                            marginRight: 5,
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+{/*                    <Typography variant="h6" noWrap component="div">
+                        Mini variant drawer
+                    </Typography>*/}
+                </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <List>
+                    <ListItem key='overview' disablePadding sx={{ display: 'block' }} onClick={() => {setMainContent(<Dashboard/>)}}>
+                            <ListItemButton
+                                sx={{
+                                    minHeight: 48,
+                                    justifyContent: open ? 'initial' : 'center',
+                                    px: 2.5,
+                                }}
                             >
-                                <MenuIcon />
-                            </IconButton>
-                        </Toolbar>
-                        <Menu
-                            anchorEl={anchorEl}
-                            open={Boolean(anchorEl)}
-                            onClose={handleMenuClose}
+                                <ListItemIcon
+                                    sx={{
+                                        minWidth: 0,
+                                        mr: open ? 3 : 'auto',
+                                        justifyContent: 'center',
+                                    }}
+                                >
+                                    <AssessmentIcon/>
+                                </ListItemIcon>
+                                <ListItemText primary='Overview' sx={{ opacity: open ? 1 : 0 }} />
+                            </ListItemButton>
+                        </ListItem>
+                    <ListItem key='transactions' disablePadding sx={{ display: 'block' }} onClick={() => {setMainContent(<TransactionStepper/>)}}>
+                        <ListItemButton
+                            sx={{
+                                minHeight: 48,
+                                justifyContent: open ? 'initial' : 'center',
+                                px: 2.5,
+                            }}
                         >
-                            <MenuItem onClick={()=> {handleMenuClose("logout")}}
+                            <ListItemIcon
+                                sx={{
+                                    minWidth: 0,
+                                    mr: open ? 3 : 'auto',
+                                    justifyContent: 'center',
+                                }}
                             >
-                                Logout
-                            </MenuItem>
-                        </Menu>
-                        <Tabs
-                            value={tab}
-                            onChange={handleTabChange}
-                            indicatorColor="secondary"
-                            textColor="inherit"
-                            centered
-                            variant="standard"
-                            aria-label="full width tabs example"
+                                <PointOfSaleIcon />
+                            </ListItemIcon>
+                            <ListItemText primary='Transactions' sx={{ opacity: open ? 1 : 0 }} />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem key='reports' disablePadding sx={{ display: 'block' }} onClick={() => {setMainContent(<Typography>reports</Typography>)}}>
+                        <ListItemButton
+                            sx={{
+                                minHeight: 48,
+                                justifyContent: open ? 'initial' : 'center',
+                                px: 2.5,
+                            }}
                         >
-                            <Tab label="Overview" value="1"/>
-                            <Tab label="Transactions" value="2"/>
-                            <Tab label="Report" value="3"/>
-                        </Tabs>
-                    </AppBar>
-                </Box>
-                <TabPanel value="1">
-                    <Dashboard/>
-                </TabPanel>
-                <TabPanel value="2">
-                    <TransactionStepper/>
-                </TabPanel>
-                <TabPanel value="3">Report</TabPanel>
-            </TabContext>
+                            <ListItemIcon
+                                sx={{
+                                    minWidth: 0,
+                                    mr: open ? 3 : 'auto',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <ArticleIcon />
+                            </ListItemIcon>
+                            <ListItemText primary='Reports' sx={{ opacity: open ? 1 : 0 }} />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+                <Divider />
+                <List>
+                    <ListItem key={'Logout'} disablePadding sx={{ display: 'block' }} onClick={handleLogout}>
+                        <ListItemButton
+                            sx={{
+                                minHeight: 48,
+                                justifyContent: open ? 'initial' : 'center',
+                                px: 2.5,
+                            }}
+                        >
+                            <ListItemIcon
+                                sx={{
+                                    minWidth: 0,
+                                    mr: open ? 3 : 'auto',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                {<LogoutIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary='Logout' sx={{ opacity: open ? 1 : 0 }} />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </Drawer>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <DrawerHeader />
+                {mainContent}
+            </Box>
         </Box>
     );
 }
