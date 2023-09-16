160febe591765a9dc48105c873228d86d911e5fc
pavelmaliy <pavel.maliy@sap.com>
2023-09-16 17:05:32 +0300

    sort table

---
+++ src/components/transactions/HistoryTable.js
@@ -0,0 +1,164 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow,
+    TableSortLabel
+} from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+const styles = {
+    tableHeaderStyle: {
+        position: 'sticky',
+        top: 0,
+        background: 'white',
+        zIndex: 1,
+    },
+    tableWrapper: {
+        position: 'relative',
+        overflowX: 'auto',
+        maxHeight: '400px', // Set the maximum height for vertical scrolling
+        overflowY: 'auto'
+    },
+    pagination: {
+        position: 'sticky',
+        bottom: 0,
+        backgroundColor: 'white', // Adjust this based on your table background
+        zIndex: 1, // Ensure it's above the table content
+    },
+};
+export function HistoryTable({history}) {
+    const rowsPerPageOptions = [5, 10, 25];
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [sortConfig, setSortConfig] = React.useState({key: '', direction: ''});
+    const [page, setPage] = React.useState(0);
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0); // Reset to the first page when changing rows per page
+    };
+
+    const handleSort = (key) => {
+        let direction = 'asc';
+        if (sortConfig.key === key && sortConfig.direction === 'asc') {
+            direction = 'desc';
+        }
+        setSortConfig({key, direction});
+    };
+
+    const sortedData = [...history].sort((a, b) => {
+        let firstCell = a[sortConfig.key]
+        let secondCell = b[sortConfig.key]
+        if (sortConfig.direction === 'asc') {
+            if (firstCell && firstCell.localeCompare) {
+                return firstCell.localeCompare(secondCell)
+            }
+            return firstCell - secondCell;
+        }
+        if (secondCell && secondCell.localeCompare) {
+            return secondCell.localeCompare(firstCell)
+        }
+        return secondCell - firstCell
+    });
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    return (
+        <TableContainer component={Paper} style={styles.tableWrapper}>
+            <Table>
+                <TableHead style={styles.tableHeaderStyle}>
+                    <TableRow>
+                        <TableCell>
+                            <TableSortLabel
+                                active={sortConfig.key === 'transactionDate'}
+                                direction={sortConfig.key === 'transactionDate' ? sortConfig.direction : 'asc'}
+                                onClick={() => handleSort('transactionDate')}
+                            >
+                                <Typography style={{fontWeight: 'bold'}}>
+                                    Date
+                                </Typography>
+                            </TableSortLabel>
+                        </TableCell>
+                        <TableCell>
+                            <TableSortLabel
+                                active={sortConfig.key === 'stockName'}
+                                direction={sortConfig.key === 'stockName' ? sortConfig.direction : 'asc'}
+                                onClick={() => handleSort('stockName')}
+                            >
+                                <Typography style={{fontWeight: 'bold'}}>
+                                    Stock
+                                </Typography>
+                            </TableSortLabel>
+                        </TableCell>
+                        <TableCell>
+                            <TableSortLabel
+                                active={sortConfig.key === 'transactionType'}
+                                direction={sortConfig.key === 'transactionType' ? sortConfig.direction : 'asc'}
+                                onClick={() => handleSort('transactionType')}
+                            >
+                                <Typography style={{fontWeight: 'bold'}}>
+                                    Action
+                                </Typography>
+                            </TableSortLabel>
+                        </TableCell>
+                        <TableCell>
+                            <TableSortLabel
+                                active={sortConfig.key === 'quantity'}
+                                direction={sortConfig.key === 'quantity' ? sortConfig.direction : 'asc'}
+                                onClick={() => handleSort('quantity')}
+                            >
+                                <Typography style={{fontWeight: 'bold'}}>
+                                    Quantity
+                                </Typography>
+                            </TableSortLabel>
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {sortedData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, index) => (
+                        <TableRow key={index}>
+                            <TableCell>
+                                <Typography>
+                                    {item.transactionDate}
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography>
+                                    {item.stockName}
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography>
+                                    {item.transactionType}
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography>
+                                    {item.quantity}
+                                </Typography>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+
+                </TableBody>
+            </Table>
+            <TablePagination
+                style={styles.pagination}
+                rowsPerPageOptions={rowsPerPageOptions}
+                component="div"
+                count={history.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </TableContainer>
+    );
+}
--- src/components/transactions/TransactionStepper.js
+++ src/components/transactions/TransactionStepper.js
@@ -11,42 +11,14 @@ import {
     Step,
     StepLabel,
     Stepper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TablePagination,
-    TableRow,
-    TableSortLabel
 } from "@mui/material";
 import {getUserTransactions, persistTransactions} from "../../storage/store"
 import {AppContext} from "../../context/AppContext";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth} from "../../storage/firebase";
+import {HistoryTable} from './HistoryTable'
 
 const steps = ['General Info', 'Stock Transactions'];
-
-const styles = {
-    tableHeaderStyle: {
-        position: 'sticky',
-        top: 0,
-        background: 'white',
-        zIndex: 1,
-    },
-    tableWrapper: {
-        position: 'relative',
-        overflowX: 'auto',
-        maxHeight: '400px', // Set the maximum height for vertical scrolling
-        overflowY: 'auto'
-    },
-    pagination: {
-        position: 'sticky',
-        bottom: 0,
-        backgroundColor: 'white', // Adjust this based on your table background
-        zIndex: 1, // Ensure it's above the table content
-    },
-};
 
 export default function TransactionStepper() {
     const {model, setModel} = useContext(AppContext);
@@ -54,10 +26,6 @@ export default function TransactionStepper() {
     const [user] = useAuthState(auth)
     const [history, setHistory] = React.useState([])
     const [loading, setLoading] = React.useState(true);
-    const rowsPerPageOptions = [5, 10, 25];
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
-    const [sortConfig, setSortConfig] = React.useState({key: '', direction: ''});
 
     useEffect(() => {
         (async () => {
@@ -70,29 +38,6 @@ export default function TransactionStepper() {
             setLoading(false);
         })()
     }, []);
-
-    const handleSort = (key) => {
-        let direction = 'asc';
-        if (sortConfig.key === key && sortConfig.direction === 'asc') {
-            direction = 'desc';
-        }
-        setSortConfig({key, direction});
-    };
-
-    const sortedData = [...history].sort((a, b) => {
-        let firstCell = a[sortConfig.key]
-        let secondCell = b[sortConfig.key]
-        if (sortConfig.direction === 'asc') {
-            if (firstCell && firstCell.localeCompare) {
-                return firstCell.localeCompare(secondCell)
-            }
-            return firstCell - secondCell;
-        }
-        if (secondCell && secondCell.localeCompare) {
-            return secondCell.localeCompare(firstCell)
-        }
-        return secondCell - firstCell
-    });
 
     function getStepContent(step) {
         switch (step) {
@@ -125,15 +70,6 @@ export default function TransactionStepper() {
                 throw new Error('Unknown step');
         }
     }
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0); // Reset to the first page when changing rows per page
-    };
 
     function NewTransaction() {
         return (
@@ -195,95 +131,7 @@ export default function TransactionStepper() {
                                 Transaction History
                             </Typography>
                             <React.Fragment>
-                                <TableContainer component={Paper} style={styles.tableWrapper}>
-                                    <Table>
-                                        <TableHead style={styles.tableHeaderStyle}>
-                                            <TableRow>
-                                                <TableCell>
-                                                    <TableSortLabel
-                                                        active={sortConfig.key === 'transactionDate'}
-                                                        direction={sortConfig.key === 'transactionDate' ? sortConfig.direction : 'asc'}
-                                                        onClick={() => handleSort('transactionDate')}
-                                                    >
-                                                        <Typography style={{fontWeight: 'bold'}}>
-                                                            Date
-                                                        </Typography>
-                                                    </TableSortLabel>
-                                                </TableCell>
-                                                <TableCell>
-                                                    <TableSortLabel
-                                                        active={sortConfig.key === 'stockName'}
-                                                        direction={sortConfig.key === 'stockName' ? sortConfig.direction : 'asc'}
-                                                        onClick={() => handleSort('stockName')}
-                                                    >
-                                                        <Typography style={{fontWeight: 'bold'}}>
-                                                            Stock
-                                                        </Typography>
-                                                    </TableSortLabel>
-                                                </TableCell>
-                                                <TableCell>
-                                                    <TableSortLabel
-                                                        active={sortConfig.key === 'transactionType'}
-                                                        direction={sortConfig.key === 'transactionType' ? sortConfig.direction : 'asc'}
-                                                        onClick={() => handleSort('transactionType')}
-                                                    >
-                                                        <Typography style={{fontWeight: 'bold'}}>
-                                                            Action
-                                                        </Typography>
-                                                    </TableSortLabel>
-                                                </TableCell>
-                                                <TableCell>
-                                                    <TableSortLabel
-                                                        active={sortConfig.key === 'quantity'}
-                                                        direction={sortConfig.key === 'quantity' ? sortConfig.direction : 'asc'}
-                                                        onClick={() => handleSort('quantity')}
-                                                    >
-                                                        <Typography style={{fontWeight: 'bold'}}>
-                                                            Quantity
-                                                        </Typography>
-                                                    </TableSortLabel>
-                                                </TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {sortedData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, index) => (
-                                                <TableRow key={index}>
-                                                    <TableCell>
-                                                        <Typography>
-                                                            {item.transactionDate}
-                                                        </Typography>
-                                                    </TableCell>
-                                                    <TableCell>
-                                                        <Typography>
-                                                            {item.stockName}
-                                                        </Typography>
-                                                    </TableCell>
-                                                    <TableCell>
-                                                        <Typography>
-                                                            {item.transactionType}
-                                                        </Typography>
-                                                    </TableCell>
-                                                    <TableCell>
-                                                        <Typography>
-                                                            {item.quantity}
-                                                        </Typography>
-                                                    </TableCell>
-                                                </TableRow>
-                                            ))}
-
-                                        </TableBody>
-                                    </Table>
-                                    <TablePagination
-                                        style={styles.pagination}
-                                        rowsPerPageOptions={rowsPerPageOptions}
-                                        component="div"
-                                        count={history.length}
-                                        rowsPerPage={rowsPerPage}
-                                        page={page}
-                                        onPageChange={handleChangePage}
-                                        onRowsPerPageChange={handleChangeRowsPerPage}
-                                    />
-                                </TableContainer>
+                                <HistoryTable history={history}/>
                             </React.Fragment>
                         </Paper>
                     </Container>
