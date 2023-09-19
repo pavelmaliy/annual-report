d16b6b6542437788b8f623c12109792ad832056e
pavelmaliy <pavel.maliy@sap.com>
2023-09-19 13:36:37 +0300

    code review

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -22,3 +22,49 @@ export const generateRandomString = function (length) {
 
     return result;
 }
+
+export const getPageFromHash = function () {
+    // Get the hash portion of the URL and remove the leading '#' character
+    const hash = window.location.hash.slice(1);
+
+// Split the hash into an array of key-value pairs
+    const keyValuePairs = hash.split('&');
+
+// Create an object to store the parsed key-value pairs
+    const parsedData = {};
+
+// Iterate through the key-value pairs and split each pair into a key and a value
+    keyValuePairs.forEach((pair) => {
+        const [key, value] = pair.split('=');
+        // Decode URI components to handle special characters
+        parsedData[key] = decodeURIComponent(value);
+    });
+
+    return (parsedData['page'] && parseInt(parsedData['page']) ) || 0
+
+}
+
+export const setPageInHash = function (page) {
+    // Get the current hash
+    const currentHash = window.location.hash.slice(1);
+
+// Parse the current hash into an object
+    const hashObject = currentHash
+        ? currentHash.split('&').reduce((acc, pair) => {
+            const [key, value] = pair.split('=');
+            acc[key] = decodeURIComponent(value);
+            return acc;
+        }, {})
+        : {};
+
+// Add or update key-value pairs
+    hashObject.page = page;
+
+
+// Convert the object back into a hash string
+// Set the updated hash to window.location.hash
+    window.location.hash = Object.entries(hashObject)
+        .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+        .join('&');
+
+}
