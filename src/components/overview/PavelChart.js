480d7fa33410ae1a9a21e57fab85ab9cd2b2424b
pavelmaliy <pavel.maliy@sap.com>
2023-12-05 09:09:04 +0200

    try charts

--- src/components/overview/PavelChart.js
+++ src/components/overview/PavelChart.js
@@ -11,45 +11,75 @@ import {
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
+    name: "Jan",
+    SAP: 4000,
+    IBM: 2400,
     amt: 2400
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
+    name: "Feb",
+    SAP: 3000,
+    IBM: 1398,
     amt: 2210
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
+    name: "Mar",
+    SAP: 2000,
+    IBM: 9800,
     amt: 2290
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
+    name: "Apr",
+    SAP: 2780,
+    IBM: 3908,
     amt: 2000
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
+    name: "May",
+    SAP: 1890,
+    IBM: 4800,
     amt: 2181
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
+    name: "Jun",
+    SAP: 2390,
+    IBM: 3800,
     amt: 2500
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
+    name: "Jul",
+    SAP: 3490,
+    IBM: 4300,
+    amt: 2100
+  },
+  {
+    name: "Aug",
+    SAP: 3490,
+    IBM: 4300,
+    amt: 2100
+  },
+  {
+    name: "Sep",
+    SAP: 3490,
+    IBM: 4300,
+    amt: 2100
+  },
+  {
+    name: "Oct",
+    SAP: 3490,
+    IBM: 4300,
+    amt: 2100
+  },
+  {
+    name: "Nov",
+    SAP: 6100,
+    IBM: 7000,
+    amt: 2100
+  },
+  {
+    name: "Dec",
+    SAP: 3490,
+    IBM: 4300,
     amt: 2100
   }
 ];
@@ -74,11 +104,11 @@ export default function Pavel() {
       <Legend />
       <Line
         type="monotone"
-        dataKey="pv"
+        dataKey="SAP"
         stroke="#8884d8"
         activeDot={{ r: 8 }}
       />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      <Line type="monotone" dataKey="IBM" stroke="#82ca9d" />
     </LineChart>
   );
 }
