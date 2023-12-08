2a8d84d8a598773bd2d322a5663c83aa70d0ad47
pavelmaliy <pavel.maliy@sap.com>
2023-12-08 10:50:31 +0200

    fix bug

--- src/components/common/InputFileUpload.js
+++ src/components/common/InputFileUpload.js
@@ -1,23 +1,37 @@
-import * as React from 'react';
+import React, { useRef } from 'react';
 import Button from '@mui/material/Button';
 import { ReactComponent as ExcelIcon } from '../../icons/excel.svg';
 import Box from "@mui/material/Box";
 
-export default function InputFileUpload({handleFileUpload}) {
+export default function InputFileUpload({ handleFileUpload }) {
+    const fileInputRef = useRef(null);
+
+    const onUpload = function (event) {
+        const selectedFile = event.target.files[0];
+        handleFileUpload(event)
+
+        if (selectedFile) {
+            if (fileInputRef.current) {
+                fileInputRef.current.value = '';
+            }
+        }
+    }
+
     return (
-        <Box display="flex" flexDirection="column" alignItems="center" style={{marginRight: '10px'}}>
+        <Box display="flex" flexDirection="column" alignItems="center" style={{ marginRight: '10px' }}>
             <input
                 type="file"
                 accept=".xlsx"
-                onChange={handleFileUpload}
-                style={{display: 'none'}}
+                onChange={onUpload}
+                style={{ display: 'none' }}
                 id="file-upload-input"
+                ref={fileInputRef}
             />
             <label htmlFor="file-upload-input">
                 <Button
                     variant="outlined"
                     component="span"
-                    startIcon={<ExcelIcon/>}
+                    startIcon={<ExcelIcon />}
                 >
                     Upload
                 </Button>
